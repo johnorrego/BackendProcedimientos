4e230954a7caa836ba113dbb4019daefbee4cf7f
johnorrego <jhon-orrego@hotmail.es>
2022-06-09 01:59:26 -0500

    Se agrega proyecto

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categoria;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categorias = Categoria::all();
+        return $categorias;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $categoria = new Categoria();
+        $categoria->nombre = $request->nombre;
+        $categoria->valor = $request->valor;
+
+        $categoria->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        $categoria = Categoria::findOrFail($request->id);
+        return $categoria;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->nombre = $request->nombre;
+        $categoria->valor = $request->valor;
+
+        $categoria->save();
+
+        return $categoria;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $categoria = Categoria::destroy($request->id);
+        return $categoria;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/categorias', 'App\Http\Controllers\CategoriaController@index');
+Route::post('/categorias', 'App\Http\Controllers\CategoriaController@store');
+Route::put('/categorias/{id}', 'App\Http\Controllers\CategoriaController@update');
+Route::delete('/categorias/{id}', 'App\Http\Controllers\CategoriaController@destroy');
+
+Route::get('/categorias/{id}', 'App\Http\Controllers\CategoriaController@show');
